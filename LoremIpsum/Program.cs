86ed5fc3c2e70d24b52a0afe6954e5ee3777660e
rafagomeznewshore <rafaelgomez@newshore.es>
2016-03-28 02:50:50 -0500

    - Caso 1: Verificar el parrafo "What is Lorem Ipsum?"
    	Pasos:
    		* Ingresar a la pagina de Lorem Ipsum
    		* Verificar que el titulo del primer parrafo es "What is Lorem Ipsum?"
    		* Verificar que el contenido del parrafo contiene el texto "Lorem Ipsum is simply dummy text of the printing and typesetting industry"

--- LoremIpsum/Program.cs
+++ LoremIpsum/Program.cs
@@ -1,26 +1,58 @@
 ﻿namespace LoremIpsum
 {
+    using NUnit.Framework;
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
-    /// 
+    /// Class program
     /// </summary>
     public class Program
     {
+        readonly IWebDriver _driver = new ChromeDriver();
+
         /// <summary>
         /// Mains the specified arguments.
         /// </summary>
         /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
 
         }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //It's timeout that i need to all element
+            //Advise me write to .config
+            //TimeSpan t = new TimeSpan(10);
+            //driver.Manage().Timeouts().ImplicitlyWait(t);
+
+            //the wait is for the elements
+
+            _driver.Navigate().GoToUrl("http://www.lipsum.com/");
+            Console.WriteLine("Open the browser");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement elementParagrafoFirst = _driver.FindElement(By.CssSelector("p"));
+            IWebElement elementTitle = elementParagrafoFirst.FindElement(By.XPath("/descendant::h2/span"));
+
+            Assert.Equals("What is Lorem Ipsum?", elementTitle.GetAttribute("innerText"));
+            StringAssert.Contains("Lorem Ipsum is simply dummy text of the printing and typesetting industry", elementParagrafoFirst.Text);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+            Console.WriteLine("Close the window");
+        }
+
+
+
     }
 }
